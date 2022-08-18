d7bb7401a47417e9e54dddde1288d5281c667003
Scott Ross <scott.william.ross2@gmail.com>
2022-08-18 18:35:49 -0400

    added dropdowns to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,53 @@
+import { BsGithub } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode, MdAdd } from "react-icons/md";
+
 const Header = () => {
+    const currentUser = "User";
+
     return (
-        <header className="sticky top-0 h-12 border-b-2">
-            <h1 className="float-left font-bold text-red p-1">
-                Formula App
-            </h1>
+        <header className="navbar border-b-2">
+            <div className="navbar-start">
+                <h1 className="font-bold text-red p-1">Formula App</h1>
+            </div>
+
+            <div className="navbar-end">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost rounded flex gap-2">
+                        <MdAdd color="#8C001D" fontSize="1.5rem" />
+                    </label>
+                    <ul tabIndex={0} className="menu dropdown-content shadow bg-base-100 rounded-box w-48 mt-4">
+                        <li><a>Create a new recipe!</a></li>
+                        <li><a>Add a new starter!</a></li>
+                    </ul>
+                </div>
+
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost rounded flex gap-2">
+                        <FaUserCircle color="#8C001D" fontSize="1.5rem" />
+                    </label>
+                    <ul tabIndex={0} className="menu dropdown-content shadow bg-base-100 rounded-box w-24 mt-4">
+                        <p className="p-4">Hi, {currentUser}!</p>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost rounded flex gap-2">
+                        <MdDarkMode color="#8C001D" fontSize="1.5rem" />
+                    </label>
+                    <ul tabIndex={0} className="menu dropdown-content shadow bg-base-100 rounded-box w-24 mt-4">
+                        <li><a>Light</a></li>
+                        <li><a>Dark</a></li>
+                        <li><a>Other Themes</a></li>
+                        <li><a>System</a></li>
+                    </ul>
+                </div>
+
+                <button className="btn btn-ghost rounded">
+                    <a href="https://github.com/scottross123/dough-formula-builder"><BsGithub color="#8C001D" fontSize="1.5rem" /></a>
+                </button>
+            </div>
         </header>
     );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,31 +4,31 @@ import { BiRestaurant, BiFoodMenu, BiPlanet, BiFile, BiHome } from "react-icons/
 const navItems: NavbarItemProps[] = [
     {
         key: "home",
-        icon: <BiHome color="#8C001D" fontSize="2rem" />,
+        icon: <BiHome color="#8C001D" fontSize="1.5rem" />,
         text: "Home",
         link: "link",
     },
     {
         key: "recipes",
-        icon: <BiRestaurant color="#8C001D" fontSize="2rem" />,
+        icon: <BiRestaurant color="#8C001D" fontSize="1.5rem" />,
         text: "Recipes",
         link: "link",
     },
     {
         key: "starters",
-        icon: <BiFoodMenu color="#8C001D" fontSize="2rem" />,
+        icon: <BiFoodMenu color="#8C001D" fontSize="1.5rem" />,
         text: "Starters",
         link: "link",
     },
     {
         key: "community",
-        icon: <BiPlanet color="#8C001D" fontSize="2rem" />,
+        icon: <BiPlanet color="#8C001D" fontSize="1.5rem" />,
         text: "Community",
         link: "link",
     },
     {
         key: "resources",
-        icon: <BiFile color="#8C001D" fontSize="2rem" />,
+        icon: <BiFile color="#8C001D" fontSize="1.5rem" />,
         text: "Resources",
         link: "link",
     },
